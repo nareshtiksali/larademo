ebb2005b9b3f0bfef56da6ad3fff0a17167eaeed
nareshtiksali <nareshtiksali@gmail.com>
2026-03-09 17:14:36 +0530

    changed fonts-size

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 padding: 0;
                 width: 100%;
                 display: table;
-                font-weight: 100;
+                font-weight: 300;
                 font-family: 'Karla';
             }
 
